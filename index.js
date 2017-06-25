59a8d1dc8cfe74a392ffeca079b5b094ff0164e1
Anh Tu <ngqanhtu@outlook.com>
2017-06-25 23:48:17 +0700

    Edit

--- index.js
+++
@@ -1,9 +0,0 @@
-
-
-var express  = require('express');
-var app      = express();
-require('./config')(app);
-var port     = process.env.PORT || 8888;
-// launch ======================================================================
-app.listen(port);
-console.log(`Port  ${port}`);
